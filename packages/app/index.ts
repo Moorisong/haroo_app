0e0bfa85cc2f4144df8b7ada85a652481430255a
Kim SongHyeon <thiagoo@naver.com>
2025-12-18 21:07:32 +0900

    feat(widget): configure widget plugin and register handler

--- packages/app/index.ts
+++ packages/app/index.ts
@@ -1,4 +1,5 @@
 import { registerRootComponent } from 'expo';
+import { Platform } from 'react-native';
 
 import App from './App';
 
@@ -6,3 +7,13 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+// Register widget task handler for Android home screen widget
+if (Platform.OS === 'android') {
+    import('react-native-android-widget').then(({ registerWidgetTaskHandler }) => {
+        import('./src/widgets/widget-task-handler').then(({ widgetTaskHandler }) => {
+            registerWidgetTaskHandler(widgetTaskHandler);
+        });
+    });
+}
+
